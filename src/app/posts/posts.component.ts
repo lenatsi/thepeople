55823d8c63fae65bb1b03199299dd20b9721f7a0
lenatsi <tsitukhinaalena@gmail.com>
2021-04-02 16:32:09 +0200

    update

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -13,49 +13,49 @@ export class PostsComponent implements OnInit {
       "title": "Bill Gates",
       "date": "28/10/1955",
       "profession":"Filántropo",
-      "imgUrl": "",
+      "imgUrl": "../../assets/img/main.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor justo a elementum consectetur. In pellentesque felis nec leo pulvinar sodales vitae ac lacus. Integer bibendum fermentum metus ac euismod. Vestibulum sapien leo, interdum in congue quis, ultricies vitae nibh. Duis vel elementum nulla. Suspendisse suscipit augue eget risus tincidunt faucibus. In id mi tellus. Vivamus egestas tellus sed tortor tincidunt, quis condimentum massa molestie. Nulla vel ornare lorem. Nam tristique porta nisl, id eleifend dui. Proin imperdiet auctor justo et imperdiet. Vivamus nunc mauris, luctus eget elementum id, pellentesque ac odio. Quisque eu eros dictum, pretium nisi in, pulvinar ex. Sed vel mi molestie, mollis ligula eu, ultrices eros. Maecenas vitae venenatis ex. Aliquam at luctus turpis. Quisque rutrum blandit convallis. Nam nec magna sed augue pretium consequat ut a arcu. Cras magna magna, varius ut tortor ut, gravida placerat turpis."
     },
     {
       "title": "Ibai Llanos",
       "date": "26/03/1995",
       "profession":"Creador contenido",
-      "imgUrl": "",
+      "imgUrl": "../../assets/img/main-3.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor justo a elementum consectetur. In pellentesque felis nec leo pulvinar sodales vitae ac lacus. Integer bibendum fermentum metus ac euismod. Vestibulum sapien leo, interdum in congue quis, ultricies vitae nibh. Duis vel elementum nulla. Suspendisse suscipit augue eget risus tincidunt faucibus. In id mi tellus. Vivamus egestas tellus sed tortor tincidunt, quis condimentum massa molestie. Nulla vel ornare lorem. Nam tristique porta nisl, id eleifend dui. Proin imperdiet auctor justo et imperdiet. Vivamus nunc mauris, luctus eget elementum id, pellentesque ac odio. Quisque eu eros dictum, pretium nisi in, pulvinar ex. Sed vel mi molestie, mollis ligula eu, ultrices eros. Maecenas vitae venenatis ex. Aliquam at luctus turpis. Quisque rutrum blandit convallis. Nam nec magna sed augue pretium consequat ut a arcu. Cras magna magna, varius ut tortor ut, gravida placerat turpis."
     },
     {
       "title": "Elon Musk",
       "date": "28/07/1971",
       "profession":"The Boss",
-      "imgUrl": "",
+      "imgUrl": "../../assets/img/main-2.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor justo a elementum consectetur. In pellentesque felis nec leo pulvinar sodales vitae ac lacus. Integer bibendum fermentum metus ac euismod. Vestibulum sapien leo, interdum in congue quis, ultricies vitae nibh. Duis vel elementum nulla. Suspendisse suscipit augue eget risus tincidunt faucibus. In id mi tellus. Vivamus egestas tellus sed tortor tincidunt, quis condimentum massa molestie. Nulla vel ornare lorem. Nam tristique porta nisl, id eleifend dui. Proin imperdiet auctor justo et imperdiet. Vivamus nunc mauris, luctus eget elementum id, pellentesque ac odio. Quisque eu eros dictum, pretium nisi in, pulvinar ex. Sed vel mi molestie, mollis ligula eu, ultrices eros. Maecenas vitae venenatis ex. Aliquam at luctus turpis. Quisque rutrum blandit convallis. Nam nec magna sed augue pretium consequat ut a arcu. Cras magna magna, varius ut tortor ut, gravida placerat turpis."
     },
     {
       "title": "Magan M.",
       "date": "28/10/1955",
       "profession":"Filántropo",
-      "imgUrl": "",
+      "imgUrl": "../../assets/img/main.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor justo a elementum consectetur. In pellentesque felis nec leo pulvinar sodales vitae ac lacus. Integer bibendum fermentum metus ac euismod. Vestibulum sapien leo, interdum in congue quis, ultricies vitae nibh. Duis vel elementum nulla. Suspendisse suscipit augue eget risus tincidunt faucibus. In id mi tellus. Vivamus egestas tellus sed tortor tincidunt, quis condimentum massa molestie. Nulla vel ornare lorem. Nam tristique porta nisl, id eleifend dui. Proin imperdiet auctor justo et imperdiet. Vivamus nunc mauris, luctus eget elementum id, pellentesque ac odio. Quisque eu eros dictum, pretium nisi in, pulvinar ex. Sed vel mi molestie, mollis ligula eu, ultrices eros. Maecenas vitae venenatis ex. Aliquam at luctus turpis. Quisque rutrum blandit convallis. Nam nec magna sed augue pretium consequat ut a arcu. Cras magna magna, varius ut tortor ut, gravida placerat turpis."
     },
     {
       "title": "Malania Gates",
       "date": "28/10/1955",
       "profession":"Filántropo",
-      "imgUrl": "",
+      "imgUrl": "../../assets/img/main.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor justo a elementum consectetur. In pellentesque felis nec leo pulvinar sodales vitae ac lacus. Integer bibendum fermentum metus ac euismod. Vestibulum sapien leo, interdum in congue quis, ultricies vitae nibh. Duis vel elementum nulla. Suspendisse suscipit augue eget risus tincidunt faucibus. In id mi tellus. Vivamus egestas tellus sed tortor tincidunt, quis condimentum massa molestie. Nulla vel ornare lorem. Nam tristique porta nisl, id eleifend dui. Proin imperdiet auctor justo et imperdiet. Vivamus nunc mauris, luctus eget elementum id, pellentesque ac odio. Quisque eu eros dictum, pretium nisi in, pulvinar ex. Sed vel mi molestie, mollis ligula eu, ultrices eros. Maecenas vitae venenatis ex. Aliquam at luctus turpis. Quisque rutrum blandit convallis. Nam nec magna sed augue pretium consequat ut a arcu. Cras magna magna, varius ut tortor ut, gravida placerat turpis."
     },
     {
       "title": "Bill Gates",
       "date": "28/10/1955",
       "profession":"Filántropo",
-      "imgUrl": "",
+      "imgUrl": "../../assets/img/main.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor justo a elementum consectetur. In pellentesque felis nec leo pulvinar sodales vitae ac lacus. Integer bibendum fermentum metus ac euismod. Vestibulum sapien leo, interdum in congue quis, ultricies vitae nibh. Duis vel elementum nulla. Suspendisse suscipit augue eget risus tincidunt faucibus. In id mi tellus. Vivamus egestas tellus sed tortor tincidunt, quis condimentum massa molestie. Nulla vel ornare lorem. Nam tristique porta nisl, id eleifend dui. Proin imperdiet auctor justo et imperdiet. Vivamus nunc mauris, luctus eget elementum id, pellentesque ac odio. Quisque eu eros dictum, pretium nisi in, pulvinar ex. Sed vel mi molestie, mollis ligula eu, ultrices eros. Maecenas vitae venenatis ex. Aliquam at luctus turpis. Quisque rutrum blandit convallis. Nam nec magna sed augue pretium consequat ut a arcu. Cras magna magna, varius ut tortor ut, gravida placerat turpis."
     },
     {
       "title": "Bill Gates",
       "date": "28/10/1955",
       "profession":"Filántropo",
-      "imgUrl": "",
+      "imgUrl": "../../assets/img/main.png",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor justo a elementum consectetur. In pellentesque felis nec leo pulvinar sodales vitae ac lacus. Integer bibendum fermentum metus ac euismod. Vestibulum sapien leo, interdum in congue quis, ultricies vitae nibh. Duis vel elementum nulla. Suspendisse suscipit augue eget risus tincidunt faucibus. In id mi tellus. Vivamus egestas tellus sed tortor tincidunt, quis condimentum massa molestie. Nulla vel ornare lorem. Nam tristique porta nisl, id eleifend dui. Proin imperdiet auctor justo et imperdiet. Vivamus nunc mauris, luctus eget elementum id, pellentesque ac odio. Quisque eu eros dictum, pretium nisi in, pulvinar ex. Sed vel mi molestie, mollis ligula eu, ultrices eros. Maecenas vitae venenatis ex. Aliquam at luctus turpis. Quisque rutrum blandit convallis. Nam nec magna sed augue pretium consequat ut a arcu. Cras magna magna, varius ut tortor ut, gravida placerat turpis."
     }
   ]

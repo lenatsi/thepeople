30b039c9cff6780f7436b0335efb65b38089f3a6
lenatsi <tsitukhinaalena@gmail.com>
2021-04-02 19:25:41 +0200

    update

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, arg: any): any {
+    if (arg === '' || arg.length < 3) return value;
+    const resultPosts = [];
+    for (const post of value){
+      if(post.title.toLowerCase().indexOf(arg.toLowerCase())> -1){
+        resultPosts.push(post)
+      }
+    }
+    return resultPosts
   }
 
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -8,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class PostsComponent implements OnInit {
 
   constructor() { }
+  filterPost ='';
   posts = [
     {
       "title": "Bill Gates",
